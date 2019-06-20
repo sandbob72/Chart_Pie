76e4361f7d7d0bd8af86b56a2d4bf514d1fdf610
SANDBOB <sakul.s2557@gmail.com>
2019-06-20 15:49:59 +0700

    Update Code

--- app/src/main/java/com/example/chart_pie/MainActivity.kt
+++ app/src/main/java/com/example/chart_pie/MainActivity.kt
@@ -25,17 +25,11 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //setContentView(R.layout.activity_main)
         val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-
 
         val data1 = 40
         val data2 = 10
         val data3 = 60
-
-        val DarkGreen = rgb(0,100,0)
-        val Red = rgb(255,0,0)
-        val DarkYellow = rgb(204,204,0)
 
         binding.text1.text = "สำเร็จ"
         binding.text2.text = "ไม่สำเร็จ"
@@ -51,7 +45,6 @@ class MainActivity : AppCompatActivity() {
     fun setupPieChartViewOne(data : Int) {
         mPie = findViewById(R.id.pie)
         mPie?.setUsePercentValues(true)
-        //mPie?.setExtraOffsets(10f, 10f, 10f, 10f)
         mPie?.setHoleRadius(75f)
         mPie?.dragDecelerationFrictionCoef
         mPie?.setTransparentCircleRadius(0f)
@@ -66,69 +59,7 @@ class MainActivity : AppCompatActivity() {
 
         val colors = ArrayList<Int>()
         val DarkGreen = rgb(0,100,0)
-
-        if (data == 0){
-            for (i in 1..10)
-            colors.add(Color.LTGRAY)
-        }
-        else if (data > 0 && data <= 10){
-            colors.add(DarkGreen)
-            for (i in 1..9)
-                colors.add(Color.LTGRAY)
-        }
-        else if (data > 10 && data <= 20){
-            for (i in 1..2)
-                colors.add(DarkGreen)
-            for (i in 1..8)
-                colors.add(Color.LTGRAY)
-        }
-        else if (data > 20 && data <= 30){
-            for (i in 1..3)
-                colors.add(DarkGreen)
-            for (i in 1..7)
-                colors.add(Color.LTGRAY)
-        }
-        else if (data > 30 && data <= 40){
-            for (i in 1..4)
-                colors.add(DarkGreen)
-            for (i in 1..6)
-                colors.add(Color.LTGRAY)
-        }
-        else if (data > 40 && data <= 50){
-            for (i in 1..5)
-                colors.add(DarkGreen)
-            for (i in 1..5)
-                colors.add(Color.LTGRAY)
-        }
-        else if (data > 50 && data <= 60){
-            for (i in 1..6)
-                colors.add(DarkGreen)
-            for (i in 1..4)
-                colors.add(Color.LTGRAY)
-        }
-        else if (data > 60 && data <= 70){
-            for (i in 1..7)
-                colors.add(DarkGreen)
-            for (i in 1..3)
-                colors.add(Color.LTGRAY)
-        }
-        else if (data > 70 && data <= 80){
-            for (i in 1..8)
-                colors.add(DarkGreen)
-            for (i in 1..2)
-                colors.add(Color.LTGRAY)
-        }
-        else if (data > 80 && data < 100){
-            for (i in 1..9)
-                colors.add(DarkGreen)
-
-            colors.add(Color.LTGRAY)
-        }
-        else if (data == 100){
-            for (i in 1..10)
-                colors.add(DarkGreen)
-
-        }
+        colors.addAll(valueData(data,DarkGreen))
 
         val dataSet = PieDataSet(entry, "")
         dataSet.setColors(colors)
@@ -155,7 +86,6 @@ class MainActivity : AppCompatActivity() {
         mPie = findViewById(R.id.pie2)
 
         mPie?.setUsePercentValues(true)
-        //mPie?.setExtraOffsets(50f, 50f, 50f, 50f)
         mPie?.setHoleRadius(75f)
 
         mPie?.dragDecelerationFrictionCoef
@@ -171,69 +101,7 @@ class MainActivity : AppCompatActivity() {
 
         val colors = ArrayList<Int>()
         val Red = rgb(255,0,0)
-
-        if (data == 0){
-            for (i in 1..10)
-                colors.add(Color.LTGRAY)
-        }
-        else if (data > 0 && data <= 10){
-            colors.add(Red)
-            for (i in 1..9)
-                colors.add(Color.LTGRAY)
-        }
-        else if (data > 10 && data <= 20){
-            for (i in 1..2)
-                colors.add(Red)
-            for (i in 1..8)
-                colors.add(Color.LTGRAY)
-        }
-        else if (data > 20 && data <= 30){
-            for (i in 1..3)
-                colors.add(Red)
-            for (i in 1..7)
-                colors.add(Color.LTGRAY)
-        }
-        else if (data > 30 && data <= 40){
-            for (i in 1..4)
-                colors.add(Red)
-            for (i in 1..6)
-                colors.add(Color.LTGRAY)
-        }
-        else if (data > 40 && data <= 50){
-            for (i in 1..5)
-                colors.add(Red)
-            for (i in 1..5)
-                colors.add(Color.LTGRAY)
-        }
-        else if (data > 50 && data <= 60){
-            for (i in 1..6)
-                colors.add(Red)
-            for (i in 1..4)
-                colors.add(Color.LTGRAY)
-        }
-        else if (data > 60 && data <= 70){
-            for (i in 1..7)
-                colors.add(Red)
-            for (i in 1..3)
-                colors.add(Color.LTGRAY)
-        }
-        else if (data > 70 && data <= 80){
-            for (i in 1..8)
-                colors.add(Red)
-            for (i in 1..2)
-                colors.add(Color.LTGRAY)
-        }
-        else if (data > 80 && data < 100){
-            for (i in 1..9)
-                colors.add(Red)
-
-            colors.add(Color.LTGRAY)
-        }
-        else if (data == 100){
-            for (i in 1..10)
-                colors.add(Red)
-
-        }
+        colors.addAll(valueData(data,Red))
 
         val dataSet = PieDataSet(entry, "")
         dataSet.setColors(colors)
@@ -261,7 +129,6 @@ class MainActivity : AppCompatActivity() {
         mPie = findViewById(R.id.pie3)
 
         mPie?.setUsePercentValues(true)
-        //mPie?.setExtraOffsets(50f, 50f, 50f, 50f)
         mPie?.setHoleRadius(75f)
 
         mPie?.dragDecelerationFrictionCoef
@@ -278,68 +145,7 @@ class MainActivity : AppCompatActivity() {
         val colors = ArrayList<Int>()
         val DarkYellow = rgb(241, 196, 15)
 
-        if (data == 0){
-            for (i in 1..10)
-                colors.add(Color.LTGRAY)
-        }
-        else if (data > 0 && data <= 10){
-            colors.add(DarkYellow)
-            for (i in 1..9)
-                colors.add(Color.LTGRAY)
-        }
-        else if (data > 10 && data <= 20){
-            for (i in 1..2)
-                colors.add(DarkYellow)
-            for (i in 1..8)
-                colors.add(Color.LTGRAY)
-        }
-        else if (data > 20 && data <= 30){
-            for (i in 1..3)
-                colors.add(DarkYellow)
-            for (i in 1..7)
-                colors.add(Color.LTGRAY)
-        }
-        else if (data > 30 && data <= 40){
-            for (i in 1..4)
-                colors.add(DarkYellow)
-            for (i in 1..6)
-                colors.add(Color.LTGRAY)
-        }
-        else if (data > 40 && data <= 50){
-            for (i in 1..5)
-                colors.add(DarkYellow)
-            for (i in 1..5)
-                colors.add(Color.LTGRAY)
-        }
-        else if (data > 50 && data <= 60){
-            for (i in 1..6)
-                colors.add(DarkYellow)
-            for (i in 1..4)
-                colors.add(Color.LTGRAY)
-        }
-        else if (data > 60 && data <= 70){
-            for (i in 1..7)
-                colors.add(DarkYellow)
-            for (i in 1..3)
-                colors.add(Color.LTGRAY)
-        }
-        else if (data > 70 && data <= 80){
-            for (i in 1..8)
-                colors.add(DarkYellow)
-            for (i in 1..2)
-                colors.add(Color.LTGRAY)
-        }
-        else if (data > 80 && data < 100){
-            for (i in 1..9)
-                colors.add(DarkYellow)
-
-            colors.add(Color.LTGRAY)
-        }
-        else if (data == 100){
-            for (i in 1..10)
-                colors.add(DarkYellow)
-
-        }
+        colors.addAll(valueData(data,DarkYellow))
 
         val dataSet = PieDataSet(entry, "")
         dataSet.setColors(colors)
@@ -359,6 +165,37 @@ class MainActivity : AppCompatActivity() {
         mPie?.setTouchEnabled(false)
 
     }
+    //--------------------------------------------------------------------------------------------------//
+    //--------------------------------------------------------------------------------------------------//
+
+    //data set color
+
+    fun valueData(data:Int,color: Int):ArrayList<Int>
+    {
+        val colors = ArrayList<Int>()
+        if (data == 0){for (i in 1..10) colors.add(Color.LTGRAY)
+        } else if (data > 0 && data <= 10){ colors.add(color)
+            for (i in 1..9) colors.add(Color.LTGRAY)
+        } else if (data > 10 && data <= 20){ for (i in 1..2) colors.add(color)
+            for (i in 1..8) colors.add(Color.LTGRAY)
+        } else if (data > 20 && data <= 30){ for (i in 1..3) colors.add(color)
+            for (i in 1..7) colors.add(Color.LTGRAY)
+        } else if (data > 30 && data <= 40){ for (i in 1..4) colors.add(color)
+            for (i in 1..6) colors.add(Color.LTGRAY)
+        } else if (data > 40 && data <= 50){ for (i in 1..5) colors.add(color)
+            for (i in 1..5) colors.add(Color.LTGRAY)
+        } else if (data > 50 && data <= 60){ for (i in 1..6) colors.add(color)
+            for (i in 1..4) colors.add(Color.LTGRAY)
+        } else if (data > 60 && data <= 70){ for (i in 1..7) colors.add(color)
+            for (i in 1..3) colors.add(Color.LTGRAY)
+        } else if (data > 70 && data <= 80){ for (i in 1..8) colors.add(color)
+            for (i in 1..2) colors.add(Color.LTGRAY)
+        } else if (data > 80 && data < 100){ for (i in 1..9) colors.add(color)
+            colors.add(Color.LTGRAY)
+        } else if (data == 100){ for (i in 1..10) colors.add(color) }
+        return colors
+    }
+    //--------------------------------------------------------------------------------------------------//
 
 }
 
